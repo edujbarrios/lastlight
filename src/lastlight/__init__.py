753d590930909ff9f1da0481ea2c889a0ee10dec
Eduardo J. Barrios <eduardojbarriosgarcia@gmail.com>
2026-09-23 14:26:36 +0100

    Export public exception types

--- src/lastlight/__init__.py
+++ src/lastlight/__init__.py
@@ -1,12 +1,17 @@
 """LastLight: offline intelligence under extreme constraints."""
 
 from .api import LastLight
+from .errors import ConfigurationError, LastLightError, PackError, PackValidationError
 from .types import QueryResult, RetrievalMetadata, SourceResult
 
 __version__ = "0.1.1"
 
 __all__ = [
+    "ConfigurationError",
     "LastLight",
+    "LastLightError",
+    "PackError",
+    "PackValidationError",
     "QueryResult",
     "RetrievalMetadata",
     "SourceResult",
