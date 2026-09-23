dcd9b321585c3311e168d659d87002ccac235ae5
Eduardo J. Barrios <eduardojbarriosgarcia@gmail.com>
2026-09-23 08:00:07 +0100

    feat: export stable public result types

--- src/lastlight/__init__.py
+++ src/lastlight/__init__.py
@@ -1,7 +1,14 @@
 """LastLight: offline intelligence under extreme constraints."""
 
 from .api import LastLight
+from .types import QueryResult, RetrievalMetadata, SourceResult
 
 __version__ = "0.1.0"
 
-__all__ = ["LastLight", "__version__"]
+__all__ = [
+    "LastLight",
+    "QueryResult",
+    "RetrievalMetadata",
+    "SourceResult",
+    "__version__",
+]
